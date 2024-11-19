a4f380d53f7dc91052c7af62afc498b53e5795c7
Bill Newman <william.newman@seattlecolleges.edu>
2024-11-19 13:56:43 -0800

    Adds daily-grind.js

---
+++ js/daily-grind.js
@@ -0,0 +1,18 @@
+myDate = new Date();
+myDay = myDate.getDay();
+
+switch(myDay){
+
+    case 0:
+        today = "Sunday";
+    break;
+
+    case 2:
+        today = "Tuesday";
+    break;
+
+    default:
+        today = "Something went wrong";
+}
+
+alert(today);
